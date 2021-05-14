1e360407fb5fff4d2908cb44e749ca374ae88f0b
kl <datesaver1234@gmail.com>
2021-05-14 18:12:01 +0900

    api

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
+    def index
+      articles = Article.order(created_at: :desc)
+      render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles }
+    end
+
+    def show
+      render json: { status: 'SUCCESS', message: 'Loaded the post', data: @post }
+    end
+
+    def create
+      article = Article.new(post_params)
+      if post.save
+        render json: { status: 'SUCCESS', data: article }
+      else
+        render json: { status: 'ERROR', data: article.errors }
+      end
+    end
+
+    def destroy
+      @article.destroy
+      render json: { status: 'SUCCESS', message: 'Deleted the post', data: @article }
+    end
+
+    def update
+      if @article.update(post_params)
+        render json: { status: 'SUCCESS', message: 'Updated the post', data: @article }
+      else
+        render json: { status: 'SUCCESS', message: 'Not updated', data: @article.errors }
+      end
+    end
+
+    private
+
+    def set_post
+      @article = Article.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title)
+    end
+end
