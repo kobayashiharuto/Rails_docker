407d2c4881c364b8cd5a1b7f60dc756363b769a9
kl <datesaver1234@gmail.com>
2021-05-15 22:47:00 +0900

    fix token

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,44 +1,45 @@
 class Api::V1::PostsController < ApplicationController
+  protect_from_forgery
   before_action :set_post, only: [:show, :update, :destroy]
 
-    def index
-      articles = Article.order(created_at: :desc)
-      render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles }
-    end
+  def index
+    articles = Article.order(created_at: :desc)
+    render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles }
+  end
 
-    def show
-      render json: { status: 'SUCCESS', message: 'Loaded the post', data: @post }
-    end
+  def show
+    render json: { status: 'SUCCESS', message: 'Loaded the post', data: @post }
+  end
 
-    def create
-      article = Article.new(post_params)
-      if post.save
-        render json: { status: 'SUCCESS', data: article }
-      else
-        render json: { status: 'ERROR', data: article.errors }
-      end
+  def create
+    article = Article.new(post_params)
+    if post.save
+      render json: { status: 'SUCCESS', data: article }
+    else
+      render json: { status: 'ERROR', data: article.errors }
     end
+  end
 
-    def destroy
-      @article.destroy
-      render json: { status: 'SUCCESS', message: 'Deleted the post', data: @article }
+  def destroy
+    @article.destroy
+    render json: { status: 'SUCCESS', message: 'Deleted the post', data: @article }
+  end
+
+  def update
+    if @article.update(post_params)
+      render json: { status: 'SUCCESS', message: 'Updated the post', data: @article }
+    else
+      render json: { status: 'SUCCESS', message: 'Not updated', data: @article.errors }
     end
+  end
 
-    def update
-      if @article.update(post_params)
-        render json: { status: 'SUCCESS', message: 'Updated the post', data: @article }
-      else
-        render json: { status: 'SUCCESS', message: 'Not updated', data: @article.errors }
-      end
-    end
+  private
 
-    private
+  def set_post
+    @article = Article.find(params[:id])
+  end
 
-    def set_post
-      @article = Article.find(params[:id])
-    end
-
-    def post_params
-      params.require(:post).permit(:title)
-    end
+  def post_params
+    params.require(:post).permit(:title)
+  end
 end
